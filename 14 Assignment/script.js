42a518361789c41103df73826c5aab3357e45e0e
Khalid Khan <121111705+Khalid3110@users.noreply.github.com>
2024-03-17 16:00:18 +0530

    add method and object property

---
+++ 14 Assignment/script.js
@@ -0,0 +1,21 @@
+let diceRoll = Math.floor(Math.random() * 6) + 1;
+console.log(diceRoll);
+
+let car = {
+  name: "Aulto",
+  model: "New",
+  color: "Black",
+};
+
+console.log(`Name of the car is ${car.name}`);
+
+let Person = {
+  name: "Khan",
+  age: 22,
+  city: "Delhi",
+};
+
+Person.city = "New York";
+Person.country = "United States";
+
+console.log(Person);
